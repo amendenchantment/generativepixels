30b4e4057aa9737c132436bb9c11a1c383bf91f8
castroploiin <fieryfire1005@gmail.com>
2023-10-02 09:30:52 -0400

    Added: imposing of functions in plot

--- p.py
+++ p.py
@@ -1,3 +1,5 @@
+from types import FunctionType
+from typing import Callable
 from PIL import Image
 import math
 from random import random
@@ -20,26 +22,28 @@ def gen_has_pixel(seed) -> list[int]:
 
     return [r, g, b]
 
-def gen_pixel(pixel: list[int]) -> list[int]:
-    function = math.sin
+def gen_pixel(pixel: list[int], function: Callable) -> list[int] | None:
     tolerance: float = 2
-    scale: float = (square_width+1)**0.5 # 0.75 acquired from trial and error
+    scale: float = (square_width+1)**0.5 # 0.5 acquired from trial and error
     center: int = square_width // 2 + 1
     try:
-        return color_pixel() if math.fabs(pixel[1] - center - scale * function((pixel[0]-center)/scale)) < tolerance else [0, 0, 0]
-    except ValueError:
-        return [0, 0, 0]
+        return color_pixel() if math.fabs(pixel[1] - center - scale * function((pixel[0]-center)/scale)) < tolerance else None
+    except:
+        return None
 
-def create_image():
+
+def create_image(functions: list[Callable]):
     global square_width
     square_width = 2**7 - 1 
-    img = Image.new('RGB', (square_width, square_width), "white")
+    img = Image.new('RGB', (square_width, square_width), "black")
     pixels = img.load()
 
-    for x in range(img.size[0]):
-        for y in range(img.size[0]):
-            color = gen_pixel([x, y])
-            pixels[x, square_width-y-1] = (color[0], color[1], color[2]) # square_width-y-1 so it iterates from the bottom of the screen instead of the top, maintaining the orientation desired
+    for function in functions:
+        for x in range(img.size[0]):
+            for y in range(img.size[0]):
+                color = gen_pixel([x, y], function)
+                if color != None:
+                    pixels[x, square_width-y-1] = (color[0], color[1], color[2]) # square_width-y-1 so it iterates from the bottom of the screen instead of the top, holding the desired orientation 
 
     increment_counter()
     current_counter = get_counter()
@@ -58,7 +62,16 @@ def get_counter() -> int:
     with open("counter.txt", "r") as file:
         return int(file.readline().strip('\x00'))
 
+# def superimpose(*args: list[FunctionType]):
+#     resultant: Callable = lambda x: 0
+#     for function in args:
+#         resultant += function
+#
+    # return resultant
+    
 def main() -> None:
-    create_image()
+    functions = [math.sin, math.tan]
+    # function = superimpose(functions)
+    create_image(functions)
 
 main() 
